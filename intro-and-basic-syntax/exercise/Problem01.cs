60afd991b3c9663de8611fbdd5556c6ed6f5a6c8
alphastation <alexander111111@gmail.com>
2018-03-21 21:52:08 +0000

    problem01 documented

--- intro-and-basic-syntax/exercise/Problem01.cs
+++ intro-and-basic-syntax/exercise/Problem01.cs
@@ -10,11 +10,13 @@ namespace CSharpBasic_SyntaxExercises
     {
         static void Main(string[] args)
         {
+            //here we receives 4 random integers on the console
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
             int d = int.Parse(Console.ReadLine());
 
+            //here we can see them printed in 4-digit debit card format
             Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", a, b, c, d);
         }
     }
