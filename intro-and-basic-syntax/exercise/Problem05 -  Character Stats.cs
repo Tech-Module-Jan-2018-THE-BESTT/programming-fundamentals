b188ac878b06a4827acae1ec13ca1e9ac2095b3c
alphastation <alexander111111@gmail.com>
2018-03-21 21:42:35 +0000

    problem05 documented

--- intro-and-basic-syntax/exercise/Problem05 -  Character Stats.cs
+++ intro-and-basic-syntax/exercise/Problem05 -  Character Stats.cs
@@ -6,18 +6,20 @@ namespace Character_Stats
     {
         static void Main(string[] args)
         {
-
+            //here we receive their name, current health, maximum health, current energy and maximum energy
             string name = Console.ReadLine();
             int currentH = int.Parse(Console.ReadLine());
             int maxH = int.Parse(Console.ReadLine());
             int currentE = int.Parse(Console.ReadLine());
             int maxE = int.Parse(Console.ReadLine());
+
+            //here we draw the corresponding health and energy wands and the remaining positions with no so called nowands=dots
             var wands = new string('|', currentH);
             var nowands = new string('.', maxH - currentH);
             var eWands = new string('|', currentE);
             var noeWands = new string('.', maxE - currentE);
 
-
+            //TODO
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: |{wands}{nowands}|");
             Console.WriteLine($"Energy: |{eWands}{noeWands}|");
@@ -26,7 +28,7 @@ namespace Character_Stats
             //Name: Mayro
              //Health: ||||||.....|
                //Energy: ||||||||||.|
-            //Console.WriteLine("Hello World!");
+            
         }
     }
 }
