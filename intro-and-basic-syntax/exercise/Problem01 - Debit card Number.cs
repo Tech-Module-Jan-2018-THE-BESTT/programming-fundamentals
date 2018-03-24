9047538194004025c7a81019b6f4781759a6af97
Nedelcho Karageorgiev <nighthawks@abv.bg>
2018-03-24 21:58:13 +0000

    fixed #1

--- intro-and-basic-syntax/exercise/Problem01 - Debit card Number.cs
+++ intro-and-basic-syntax/exercise/Problem01 - Debit card Number.cs
@@ -17,7 +17,7 @@ namespace CSharpBasic_SyntaxExercises
             int num4 = int.Parse(Console.ReadLine());
 
             //here we can see them printed in 4-digit debit card format
-            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", a, b, c, d);
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", num1, num2, num3, num4);
         }
     }
 }
