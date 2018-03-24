2fce20c747d2f3b2a86313bfe4c59a9cf2e441d1
Nedelcho Karageorgiev <nighthawks@abv.bg>
2018-03-24 11:38:19 +0000

    Problem 03. Documented successfully

--- intro-and-basic-syntax/exercise/Problem03.cs
+++ intro-and-basic-syntax/exercise/Problem03.cs
@@ -6,9 +6,13 @@ namespace Area
     {
         static void Main(string[] args)
         {
+            // Initially we receive the parameter: input miles
             var miles = double.Parse(Console.ReadLine());
+
+            // Next step we convert the input miles to kilometers
             var kilometers = miles * 1.60934;
 
+            // Finally we print the result formated to the second decimal place.
             Console.WriteLine($"{kilometers:F2}");
         }
     }
