3a2f29ff0d06cd3da99042635d92f2531d50e271
LilyanaAngelova <lilyana.ang.valcheva@gmail.com>
2018-03-24 00:12:45 -0700

    add problem 3

---
+++ intro-and-basic-syntax/exercise/Problem03.cs
@@ -0,0 +1,15 @@
+﻿uusing System;
+
+namespace Area
+{
+    class Problem_3
+    {
+        static void Main(string[] args)
+        {
+            var miles = double.Parse(Console.ReadLine());
+            var kilometers = miles * 1.60934;
+
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
