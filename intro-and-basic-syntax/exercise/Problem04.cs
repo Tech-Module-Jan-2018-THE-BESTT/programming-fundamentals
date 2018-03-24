27073a0ddce457022b8e19e638c85fb781715e75
LilyanaAngelova <lilyana.ang.valcheva@gmail.com>
2018-03-24 00:23:00 -0700

    add Problem 4

---
+++ intro-and-basic-syntax/exercise/Problem04.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Area
+{
+    class Problem04
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var sugar = int.Parse(Console.ReadLine());
+            var enrgyVolume = volume * energy / 100.0;
+            var sugarVolume = volume * sugar / 100.0;
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{enrgyVolume}kcal, {sugarVolume}g sugars");
+        }
+    }
+}
