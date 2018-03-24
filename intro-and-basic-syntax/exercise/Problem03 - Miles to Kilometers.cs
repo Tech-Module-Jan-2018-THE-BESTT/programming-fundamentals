3583b23e3e996730f25802dc1f159850a26f8c81
alphastation <alexander111111@gmail.com>
2018-03-24 20:53:03 +0000

    3 exercises named

---
+++ intro-and-basic-syntax/exercise/Problem03 - Miles to Kilometers.cs
@@ -0,0 +1,19 @@
+﻿uusing System;
+
+namespace Area
+{
+    class Problem_3
+    {
+        static void Main(string[] args)
+        {
+            // Initially we receive the parameter: input miles
+            var miles = double.Parse(Console.ReadLine());
+
+            // Next step we convert the input miles to kilometers
+            var kilometers = miles * 1.60934;
+
+            // Finally we print the result formated to the second decimal place.
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
---
+++ intro-and-basic-syntax/exercise/Problem04 - Bevarage Labels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Area
+{
+    class Problem04
+    {
+        static void Main(string[] args)
+        {
+            //here we receives 1 string and  3 random integers on the console
+            string drinkname = Console.ReadLine();
+            var volume = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var sugar = int.Parse(Console.ReadLine());
+
+            //we calculated the energy and sugar content for the given volume 
+            var enrgyVol = volume * energy / 100.0;
+            var sugarVol = volume * sugar / 100.0;
+
+            //we printed  Volume – integer, suffixed by “ml”, Name and next line: Energy content – integer, suffixed by “kcal” , Sugar content – integer, suffixed by “g” 
+            Console.WriteLine($"{volume}ml {drinkname}:");
+            Console.WriteLine($"{enrgyVol}kcal, {sugarVol}g sugars");
+        }
+    }
+}
