e7d1ca39e8baa525ede684b26a115bc9430c61b5
LilyanaAngelova <lilyana.ang.valcheva@gmail.com>
2018-03-24 00:51:11 -0700

    Member 3 Document the code of the NEXT Problem and problem 2 too
    Signed-off-by: LilyanaAngelova <lilyana.ang.valcheva@gmail.com>

--- intro-and-basic-syntax/exercise/Problem02 - Rectangle Area.cs
+++ intro-and-basic-syntax/exercise/Problem02 - Rectangle Area.cs
@@ -6,14 +6,14 @@ namespace Problem_2._Rectangle_Area
     {
         static void Main(string[] args)
         {
+            //we receives width and height far  -  integers  on the console  
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
 
+            //we calculated area
             double area = width * height;
-            
 
-
-
+            ////we printed in format
             Console.WriteLine($"{area:f2}");
         }
     }
--- intro-and-basic-syntax/exercise/Problem04.cs
+++ intro-and-basic-syntax/exercise/Problem04.cs
@@ -6,12 +6,17 @@ namespace Area
     {
         static void Main(string[] args)
         {
+            //here we receives 1 string and  3 random integers on the console
             string name = Console.ReadLine();
             var volume = int.Parse(Console.ReadLine());
             var energy = int.Parse(Console.ReadLine());
             var sugar = int.Parse(Console.ReadLine());
+
+            //we calculated the energy and sugar content for the given volume 
             var enrgyVolume = volume * energy / 100.0;
             var sugarVolume = volume * sugar / 100.0;
+
+            //we printed  Volume – integer, suffixed by “ml”, Name and next line: Energy content – integer, suffixed by “kcal” , Sugar content – integer, suffixed by “g” 
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{enrgyVolume}kcal, {sugarVolume}g sugars");
         }
